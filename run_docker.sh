a27a3b43d006844e624506f1bfb0b17a84b6e130
pythonfullstack <chenfang199531@gmail.com>
2021-07-31 02:10:08 +0800

    bash

--- run_docker.sh
+++ run_docker.sh
@@ -1,8 +1,19 @@
+
+
+
 echo killing old docker processes
 docker-compose rm -fs
 
 echo building docker containers
 docker-compose build
+
+echo migrating
+docker-compose exec web sh
+export FLASK_CONFIGURATION=production
+flask db init
+flask db migrate
+flask db upgrade
+exit
 
 echo running docker containers
 docker-compose up
