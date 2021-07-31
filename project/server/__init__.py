e0f85f33509f6c1676453d1af53ca1683b302a94
pythonfullstack <chenfang199531@gmail.com>
2021-08-01 00:32:43 +0800

    docker-compose

--- project/server/__init__.py
+++ project/server/__init__.py
@@ -5,17 +5,18 @@ from project.server import config
 from project.server import controllers
 from project.server.database import db
 
-
 migrate = Migrate()
 
 
 def create_app(environment):
     app = Flask(__name__)
+    app.config.from_object(config.app_config[environment])
+    print(environment, config.app_config[environment]
+          )
     app.config.update(
         CELERY_BROKER_URL='redis://localhost:6379',
         CELERY_RESULT_BACKEND='redis://localhost:6379'
     )
-    app.config.from_object(config.app_config[environment])
     database.init_app(app)
     controllers.init_app(app)
     migrate.init_app(app, db)
